d24ca029dafe828ec9ecd4f26cb55c07de8ad16d
lizhengxiang <1065482100@qq.com>
2016-07-21 06:46:11 +0800

    xxx

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
-$serv = new swoole_server("127.0.0.1", 9501); 
+$serv = new swoole_server("127.0.0.1", 9501);
 
 //监听连接进入事件
-$serv->on('connect', function ($serv, $fd) {  
+$serv->on('connect', function ($serv, $fd) {
     echo "Client: Connect.\n";
 });
 
@@ -17,4 +17,4 @@ $serv->on('close', function ($serv, $fd) {
 });
 
 //启动服务器
-$serv->start(); 
+$serv->start();
